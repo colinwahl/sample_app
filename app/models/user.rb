b09f79c2d2d9f2cce22debbe9e44ae05c8744cfc
Colin Wahl <colin.t.wahl@gmail.com>
2015-09-02 23:32:54 -0400

    working on chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                                     uniqueness: { case_sensitive: false }
-  validates :password,presence: true, length: { minimum: 6 }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def self.digest(string)
